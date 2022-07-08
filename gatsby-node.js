257b00fae40f29482d5f028e033f5dad540d7089
Kyle Mathews <mathews.kyle@gmail.com>
2022-07-08 16:58:36 -0700

    Add new redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,5 +2,6 @@
 exports.createPages = ({ graphql, actions }) => {
   const { createRedirect } = actions
   createRedirect({ fromPath: '/old-url', toPath: '/', isPermanent: true })
+  createRedirect({ fromPath: '/old-url2', toPath: '/', isPermanent: true })
   // Create pages here
 }
