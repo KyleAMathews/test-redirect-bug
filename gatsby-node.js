3f57fde96daff3941cb0328818e5e5e106236ee4
Kyle Mathews <mathews.kyle@gmail.com>
2022-07-08 17:00:17 -0700

    more redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,7 @@ exports.createPages = ({ graphql, actions }) => {
   const { createRedirect } = actions
   createRedirect({ fromPath: '/old-url', toPath: '/', isPermanent: true })
   createRedirect({ fromPath: '/old-url2', toPath: '/', isPermanent: true })
+  createRedirect({ fromPath: '/old-url3', toPath: '/', isPermanent: true })
+  createRedirect({ fromPath: '/old-url4', toPath: '/', isPermanent: true })
   // Create pages here
 }
